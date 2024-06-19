00fe7e200761dde46b1aceb9aecb193094daa172
yeonsu <rhdustn@imantisco.com>
2024-06-19 16:34:59 +0900

    feat : update sns images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,12 @@
-export { default as mainLogo } from "./main_logo.png";
-export { default as mainBg } from "./main_bg.jpg";
-export { default as iconEpic } from "./icon_epic_game.png";
-export { default as mantiscoLogo } from "./logo_mantisco.png";
-export { default as megaLink } from "./logo_megalink.png";
-export { default as iconDeco } from "./icon_title.png";
-export { default as iconDiscord } from "./icon_discord.svg";
-export { default as iconYoutube } from "./icon_youtube.svg";
+export { default as mainLogo } from './main_logo.png';
+export { default as mainBg } from './main_bg.jpg';
+export { default as iconEpic } from './icon_epic_game.png';
+export { default as mantiscoLogo } from './logo_mantisco.png';
+export { default as megaLink } from './logo_megalink.png';
+export { default as iconDeco } from './icon_title.png';
+export { default as iconDiscord } from './icon_discord.svg';
+export { default as iconYoutube } from './icon_youtube.svg';
+export { default as iconX } from './icon_x.svg';
 
 // screen shots
-export { default as galleryBg } from "./gallery/bg_gallery.jpg";
+export { default as galleryBg } from './gallery/bg_gallery.jpg';
